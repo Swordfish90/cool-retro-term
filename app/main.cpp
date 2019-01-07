ade36c013b749006bee34830ec196148e1559cb9
Sascha Paunovic <azarus@posteo.net>
2019-01-07 12:24:34 +0100

    correct version number output with --version

--- app/main.cpp
+++ app/main.cpp
@@ -71,7 +71,7 @@ int main(int argc, char *argv[])
     }
 
     if (args.contains("-v") || args.contains("--version")) {
-        qDebug() << "cool-retro-term 1.0.1";
+        qDebug() << "cool-retro-term 1.1.0";
 	return 0;
     }
 
